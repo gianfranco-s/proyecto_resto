039ee128baf48d89b9216a521fe62694d37143c3
gsalomone <gianfranco.s@gmail.com>
2024-01-23 14:50:13 -0300

    Product viewset definition

--- server/products/views.py
+++ server/products/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+
+class ProductViewSet(viewsets.ViewSet):
+    def list(self, request):  # /api/products [GET]
+        pass
+
+    def create(self, request):  # /api/products [POST]
+        pass
+
+    def retrieve(self, request, pk = None):  # /api/products/<str:id> [GET]
+        pass
+
+    def update(self, request, pk = None):  # /api/products/<str:id> [PUT]
+        pass
+
+    def delete(self, request, pk = None):  # /api/products/<str:id> [DELETE]
+        pass
