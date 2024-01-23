83ed243fa8f39eab3b0522c245670e488c1e6162
gsalomone <gianfranco.s@gmail.com>
2024-01-23 16:44:15 -0300

    Destroy method

--- server/products/views.py
+++ server/products/views.py
@@ -31,5 +31,6 @@ class ProductViewSet(viewsets.ViewSet):
 
 
     def destroy(self, request, pk = None):  # /api/products/<str:pk> [DELETE]
-        pass
-
+        product = Product.objects.get(id=pk)
+        product.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
