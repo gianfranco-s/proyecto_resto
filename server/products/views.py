673620603746da43f9a5f7cf29f33e45f2b31081
gsalomone <gianfranco.s@gmail.com>
2024-01-23 16:13:24 -0300

    Methods list and create for ProductViewSet

--- server/products/views.py
+++ server/products/views.py
@@ -1,12 +1,23 @@
-from rest_framework import viewsets
+from rest_framework import status, viewsets
+from rest_framework.response import Response
 
+
+from .models import Product
+from .serializers import ProductSerializer
 
 class ProductViewSet(viewsets.ViewSet):
     def list(self, request):  # /api/products [GET]
-        pass
+        products = Product.objects.all()
+        serializer = ProductSerializer(products, many=True)
+        return Response(serializer.data)
 
     def create(self, request):  # /api/products [POST]
-        pass
+        serializer = ProductSerializer(data=request.data)
+        print('--------')
+        print(serializer.__dict__)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
 
     def retrieve(self, request, pk = None):  # /api/products/<str:id> [GET]
         pass
